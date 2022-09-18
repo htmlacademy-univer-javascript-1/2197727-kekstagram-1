48c8ee0a2206545bce63168ffdbfb945dae8ca69
Ольга <zagirova2070@mail.ru>
2022-09-18 23:18:02 +0500

    Подключает файл со стрелочными функциями

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-function getNumber (begin, end) {
+const getNumber = (begin, end) => {
   return (begin >= 0 && end >= begin) ? Math.floor(Math.random() * (begin - end + 1)) + end : 'Ошибка';
 }
 console.log(getNumber(1, 6));
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function stringLength (verifiedString, maxLine) {
+const stringLength = (verifiedString, maxLine) => {
   return (verifiedString.length <= maxLine) ? true : false;
 }
 console.log(stringLength("Кекс", 6));
