78840bd6871b84ea2cdf0e23e7f35574038d4ec3
Ольга <zagirova2070@mail.ru>
2022-09-19 11:32:18 +0500

    доработки

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-const getNumber = (begin, end) => {
-  return (begin >= 0 && end >= begin) ? Math.floor(Math.random() * (begin - end + 1)) + end : 'Ошибка';
-}
+const getNumber = (begin, end) => (begin >= 0 && end >= begin) ? Math.floor(Math.random() * (begin - end + 1)) + end : 'Ошибка';
 console.log(getNumber(1, 6));
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-const getStringLength = (verifiedString, maxLine) => {
-  return (verifiedString.length <= maxLine) ? true : false;
-}
-console.log(getStringLength("Кекс", 6));
+const checkMaXLengthString = (string, maxCountSymbol) => string.length < maxCountSymbol;
+console.log(checkMaXLengthString("Кекс", 7));
