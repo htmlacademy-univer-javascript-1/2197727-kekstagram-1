dd76b1545bcb1ffccb371a22074700941614f712
Ольга <zagirova2070@mail.ru>
2022-12-18 19:04:02 +0500

    Отправка на сервер

--- js/consts.js
+++ js/consts.js
@@ -1,51 +1,9 @@
-export const NAMES = [
-  'Аркадий Шапулов',
-  'Пион Розевский',
-  'Нагарья Ларузьева',
-  'Полина Завиная',
-  'Страх Божий',
-  'Марина Яшко',
-  'Град Марсович',
-  'Лона Поло',
-  'Малина Малинская'
-];
-
-export const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-export const DESCRIPTIONS = [
-  'Вперед и только вперед!',
-  'Лучшее время...',
-  'Когда пошли за цветами, но купили не цветы'
-];
-
-export const CountLike = {
-  MIN: 15,
-  MAX: 200
-};
-
-export const AvatarNumber = {
-  MIN: 1,
-  MAX: 6
-};
-
-export const CountComment = {
-  MIN: 0,
-  MAX: 23
-};
-
 export const DEFAULT_RENDERED_COMMENTS = 5;
-export const MAX_COUNT_PHOTOS = 25;
 export const STEP_ADDED_COMMENTS = 5;
 export const MAX_LENGTH_STRING = 140;
 export const MAX_COUNT_HASHTAG = 5;
 export const MAX_LENGTH_HASHTAG = 20;
+export const ALERT_SHOW_TIME = 5000;
 
 export const ErrorMessage = {
   START_WITH: 'Хэш-тег должен начинаться с символа #',
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-import { createPhotos } from './data.js';
 import { initThumbnails } from './thumbnails.js';
-import { renderUploadForm, initEffects } from './form.js';
+import { renderUploadForm } from './form.js';
+import { getData } from './api.js';
+import { showAlert } from './utils.js';
+import { ALERT_SHOW_TIME } from './consts.js';
 
-const data = createPhotos();
+getData((photos) => initThumbnails(photos),
+  () => showAlert('Не удалось загрузить фотографии, обновите страницу', ALERT_SHOW_TIME),
+);
 
-initThumbnails(data);
-renderUploadForm(data);
-initEffects(data);
+renderUploadForm();
